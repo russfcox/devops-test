7d0feb46f9666bbd8231a5a402c36fa537692e68
Russ Cox <russell.cox@aexp.com>
2017-11-14 10:15:50 +0000

    circle syntax fix

---
+++ app2/migrations/sqls/20171114090523-seed-up.sql
@@ -0,0 +1,21 @@
+/* Replace with your SQL commands */SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+CREATE TABLE IF NOT EXISTS `t_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+INSERT INTO `t_user` (`user_id`, `name`, `email`, `password`) VALUES
+(1, 'Mas Banyar', 'banyar@yahoo.com', '23235645yghgf'),
+(2, 'Mas Mapmup', 'mapmup@gmail.com', 'dncskdcndscsdcdsc'),
+(4, 'Boronong', 'borononn@yahoo.com', '032bcsjdncsdjc3223'),
+(5, 'Nadya Ek', 'nadya@yahoo.com', 'bonbon032932');
+
+
+INSERT INTO `t_user` (`user_id`, `name`, `email`, `password`) VALUES
+(6, 'Russ Cox', 'russfcox@gmail.com', 'testpass');
