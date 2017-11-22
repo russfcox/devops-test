1c2caca22a2bedcb964b299bd512a0e8ba935b9c
Russ Cox <russell.cox@aexp.com>
2017-11-22 10:59:51 +0000

    added terraform deploy to circle

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -65,5 +65,5 @@ variable "instance_type" {
 
 variable "ssh_pubkey_file" {
     description = "Path to an SSH public key"
-    default = "~/.ssh/id_rsa.pub"
+    default = "~/.ssh/id_rsa.aws.pub"
 }
