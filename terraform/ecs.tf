b19eaca746d36135baa29d889682ec7c079506f0
Russ Cox <russell.cox@aexp.com>
2018-01-19 11:50:20 +0000

    added ecs service autoscaling config to terraform

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -23,3 +23,110 @@ resource "aws_launch_configuration" "ecs" {
     associate_public_ip_address = true
     user_data = "#!/bin/bash\necho ECS_CLUSTER='ECS${terraform.workspace}' > /etc/ecs/ecs.config"
 }
+
+# Cloudwatch Alarms for triggering autoscaling
+resource "aws_cloudwatch_metric_alarm" "app_cpu_high" {
+  alarm_name           = "app_cpu_high_${terraform.workspace}"
+  comparison_operator  = "GreaterThanOrEqualToThreshold"
+  evaluation_periods   = "1"
+  metric_name          = "CPUUtilization"
+  namespace            = "AWS/ECS"
+  period               = "60"
+  statistic            = "Average"
+  threshold            = "70"
+  dimensions {
+    ClusterName = "${aws_ecs_cluster.main.name}"
+    ServiceName = "${aws_ecs_service.test-http.name}"
+  }
+  alarm_actions = ["${aws_appautoscaling_policy.app_up.arn}"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "app_cpu_low" {
+  alarm_name           = "app_cpu_low_${terraform.workspace}"
+  comparison_operator  = "LessThanThreshold"
+  evaluation_periods   = "1"
+  metric_name          = "CPUUtilization"
+  namespace            = "AWS/ECS"
+  period               = "60"
+  statistic            = "Average"
+  threshold            = "20"
+  dimensions {
+    ClusterName = "${aws_ecs_cluster.main.name}"
+    ServiceName = "${aws_ecs_service.test-http.name}"
+  }
+  alarm_actions = ["${aws_appautoscaling_policy.app_down.arn}"]
+}
+
+# Autoscale Target
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 4
+  min_capacity       = 2
+  resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.test-http.name}"
+  # role_arn           = "arn:aws:iam::373776389706:role/ecsAutoscaleRole"
+  role_arn           = "${aws_iam_role.ecs_service_role.arn}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+
+  depends_on = [
+    "aws_ecs_cluster.main",
+    "aws_ecs_service.test-http"
+  ]
+}
+
+# Autoscaling Policies
+
+resource "aws_appautoscaling_policy" "app_up" {
+  name                      = "app_scale_up_${terraform.workspace}"
+  service_namespace         = "ecs"
+  resource_id               = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.test-http.name}"
+  scalable_dimension        = "ecs:service:DesiredCount"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = "300"
+    metric_aggregation_type = "Maximum"
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = 1
+    }
+  }
+  depends_on = ["aws_appautoscaling_target.ecs_target"]
+}
+
+resource "aws_appautoscaling_policy" "app_down" {
+  name                      = "app_scale_down_${terraform.workspace}"
+  service_namespace         = "ecs"
+  resource_id               = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.test-http.name}"
+  scalable_dimension        = "ecs:service:DesiredCount"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = "300"
+    metric_aggregation_type = "Maximum"
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = -1
+    }
+  }
+  depends_on = ["aws_appautoscaling_target.ecs_target"]
+}
+
+# resource "aws_appautoscaling_policy" "ecs_policy" {
+#   name                    = "scale-down"
+#   resource_id             = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.test-http.name}"
+#   scalable_dimension      = "ecs:service:DesiredCount"
+#   service_namespace       = "ecs"
+#
+#   step_scaling_policy_configuration {
+#     adjustment_type         = "ChangeInCapacity"
+#     cooldown                = 60
+#     metric_aggregation_type = "Maximum"
+#
+#     step_adjustment {
+#       metric_interval_upper_bound = 0
+#       scaling_adjustment          = -1
+#     }
+#   }
+#
+#   depends_on = ["aws_appautoscaling_target.ecs_target"]
+# }
--- terraform/services.tf
+++ terraform/services.tf
@@ -43,7 +43,7 @@ resource "aws_ecs_service" "test-http" {
     cluster = "${aws_ecs_cluster.main.id}"
     task_definition = "${aws_ecs_task_definition.test-http.arn}"
     iam_role = "${aws_iam_role.ecs_service_role.arn}"
-    desired_count = 1
+    desired_count = 2
     depends_on = ["aws_iam_role_policy.ecs_service_role_policy"]
 
     load_balancer {
@@ -52,32 +52,3 @@ resource "aws_ecs_service" "test-http" {
         container_port = 3000
     }
 }
-
-resource "aws_appautoscaling_target" "ecs_target" {
-  max_capacity       = 4
-  min_capacity       = 2
-  resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.test-http.name}"
-  role_arn           = "arn:aws:iam::373776389706:role/ecsAutoscaleRole"
-  scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
-}
-
-# resource "aws_appautoscaling_policy" "ecs_policy" {
-#   name                    = "scale-down"
-#   resource_id             = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.test-http.name}"
-#   scalable_dimension      = "ecs:service:DesiredCount"
-#   service_namespace       = "ecs"
-#
-#   step_scaling_policy_configuration {
-#     adjustment_type         = "ChangeInCapacity"
-#     cooldown                = 60
-#     metric_aggregation_type = "Maximum"
-#
-#     step_adjustment {
-#       metric_interval_upper_bound = 0
-#       scaling_adjustment          = -1
-#     }
-#   }
-#
-#   depends_on = ["aws_appautoscaling_target.ecs_target"]
-# }
